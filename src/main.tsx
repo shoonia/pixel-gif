d056aaded670f0b43f47b1799607826768643dc0
shoonia <alexanderz@wix.com>
2023-02-22 13:53:37 +0200

    Add initial color

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { App } from './components/App';
-import { readyStore } from './store';
+import { dispatch, readyStore } from './store';
+import { parseHex, randomHex } from './util';
+
+const [isValid, color] = parseHex(location.hash);
+const hex = isValid ? color : randomHex(6);
+
+history.pushState('', '', '#' + hex);
+dispatch('set/hex', hex);
 
 document.body.append(<App />);
 
