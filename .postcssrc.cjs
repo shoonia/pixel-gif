20e22bfa2c5c59ba3c6c40c5903362fe1a23b7dc
shoonia <alexanderz@wix.com>
2023-02-23 23:38:41 +0200

    Fix build

--- .postcssrc.cjs
+++ .postcssrc.cjs
@@ -1,15 +1,15 @@
-const generateScopedName = require('mini-css-class-name/postcss-modules');
+const miniCssClassName = require('mini-css-class-name/postcss-modules');
 
-const isDev = process.env.NODE_ENV === 'development';
+const generateScopedName = process.env.NODE_ENV === 'development'
+  ? '[folder]_[local]_[hash:base64:5]'
+  : miniCssClassName();
 
 module.exports = {
   modules: true,
   plugins: {
     'postcss-input-range': {},
     'postcss-modules': {
-      generateScopedName: isDev
-        ? '[folder]_[local]_[hash:base64:5]'
-        : generateScopedName(),
+      generateScopedName,
     },
   },
 };
