a96f011cfeef76731757e276c7ac64b7533a1c5f
shoonia <alexanderz@wix.com>
2023-02-24 00:19:00 +0200

    Add styles

--- src/components/Download/index.tsx
+++ src/components/Download/index.tsx
@@ -1,16 +1,23 @@
 import type { FC } from 'jsx-dom-runtime';
 
+import * as s from './styles.module.css';
 import { saveGifButton, saveGifLink } from './save';
 
 export const Download: FC = () => {
-  return typeof window.showSaveFilePicker === 'function'
+  const button = typeof window.showSaveFilePicker === 'function'
     ? (
-      <button type="button" onclick={saveGifButton}>
+      <button type="button" class={s.btn} onclick={saveGifButton}>
         Download
       </button>
     ) : (
-      <a ref={saveGifLink} href="#" role="button">
+      <a role="button" class={s.btn} ref={saveGifLink} href="#">
         Download
       </a>
     );
+
+  return (
+    <fieldset>
+      {button}
+    </fieldset>
+  );
 };
