e1a0b2074445463dbdaca13c3b08f2bf4b7ea418
shoonia <alexanderz@wix.com>
2023-04-04 21:01:16 +0300

    Add history

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,27 @@
 import './global.css';
 import { App } from './components/App';
-import { readyStore } from './store';
+import { dispatch, getState, readyStore } from './store';
+import { parseHex, randomHex } from './util';
+
+const color = parseHex(location.hash);
+const hex = color ? color : randomHex(6);
+
+history.pushState('', '', '#' + hex);
+
+window.addEventListener('popstate', () => {
+  const state = getState();
+  const hash = location.hash.slice(1);
+
+  if (hash !== state.hex) {
+    const i = parseHex(hash);
+
+    if (i) {
+      dispatch('hex', i);
+    }
+  }
+});
 
 document.body.append(<App />);
+
+dispatch('hex', hex);
 readyStore();
