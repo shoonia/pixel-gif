9ce7e1c08d84c6cef9d0133689fb4170fbd9b02f
Alexander Zaytsev <zaytsev126@gmail.com>
2023-04-06 07:33:32 +0300

    Fix title

--- src/components/RgbInputs/index.tsx
+++ src/components/RgbInputs/index.tsx
@@ -2,7 +2,7 @@ import { Group } from '../Group';
 import { PairInputs } from './PairInputs';
 
 export const RGBInputs: FC = () => (
-  <Group open title="RGBA">
+  <Group open title="RGB">
     <PairInputs param="r" />
     <PairInputs param="g" />
     <PairInputs param="b" />
