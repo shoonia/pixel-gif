0843c29c41541e0bf23413ad3616fed36455a34f
shoonia <alexanderz@wix.com>
2023-04-10 15:16:35 +0300

    Add analytics script

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import './global.css';
 import { App } from './components/App';
+import { Analytics } from './components/Analytics';
 import { dispatch, readyStore } from './store';
 import { parseHex, randomHex } from './util';
 
@@ -10,3 +11,4 @@ dispatch('hex', hex);
 
 history.pushState('', '', '#' + hex);
 document.body.append(<App ready={readyStore} />);
+document.head.append(<Analytics />);
