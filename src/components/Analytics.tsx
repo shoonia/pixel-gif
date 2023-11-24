1adf5456e9302662c4004d5b795cd458d9307b4e
shoonia <alexanderz@wix.com>
2023-11-24 19:52:20 +0200

    Refactoring

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,7 +1,7 @@
 window.dataLayer = [];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function gtag(a: unknown, b: unknown): void {
+function gtag(a: string, b: unknown): void {
   // eslint-disable-next-line prefer-rest-params
   window.dataLayer.push(arguments);
 }
