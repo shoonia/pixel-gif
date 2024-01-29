bb85f77d2880bd2b0dfe845788dd103c120c82b1
shoonia <alexanderz@wix.com>
2024-01-29 16:45:57 +0200

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -57,9 +57,6 @@ export default ({ NODE_ENV }) => {
             ecma: 2020,
             module: true,
             toplevel: true,
-            parse: {
-              ecma: 2020,
-            },
             compress: {
               ecma: 2020,
               module: true,
@@ -69,10 +66,10 @@ export default ({ NODE_ENV }) => {
               passes: 3,
               toplevel: true,
               pure_getters: true,
-            },
-            output: {
-              ecma: 2020,
-              comments: false,
+              unsafe_arrows: true,
+              unsafe_undefined: true,
+              unsafe_math: true,
+              unsafe_symbols: true,
             },
           },
         }),
