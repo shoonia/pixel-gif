a4b0279ca1e2fc6afa2a6764e16813c12b8fcdb0
shoonia <alexanderz@wix.com>
2025-05-14 17:59:51 +0300

    Update link

--- src/components/PixelPNG/index.tsx
+++ src/components/PixelPNG/index.tsx
@@ -1,18 +1,16 @@
 import s from './styles.css';
 import { connect } from '../../store';
 
-const url = 'https://shoonia.github.io/1x1/#';
-
 export const PixelPNG: JSX.FC = () => {
   const ready: JSX.Ref<HTMLAnchorElement> = (a) =>
     connect('hex', (state) => {
-      a.href = url + state.hex + 'ff';
+      a.hash = state.hex + 'ff';
     });
 
   return (
     <a
       ref={ready}
-      href={url}
+      href="https://shoonia.github.io/1x1/"
       class={s.link}
       aria-label="One pixel Base64 encoded transparent PNG generator"
     >
